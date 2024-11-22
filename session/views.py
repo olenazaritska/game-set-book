8fd252856b8b16b5cad1b04718b3b00a046fd53c
Olena Zaritska <zaritskaos@gmail.com>
2024-11-22 14:56:00 +0200

    add SessionViewSet with list action

--- session/views.py
+++ session/views.py
@@ -2,12 +2,20 @@ from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
 from session.models import SportClub
-from session.serializers import SportClubSerializer
+from session.serializers import (
+    SportClubSerializer,
+    SportClubSessionsSerializer,
+)
 
 
 class SportClubViewSet(
     mixins.ListModelMixin,
     GenericViewSet,
 ):
-    queryset = SportClub.objects.all()
+    queryset = SportClub.objects.prefetch_related("working_hours")
     serializer_class = SportClubSerializer
+
+
+class SessionViewSet(mixins.ListModelMixin, GenericViewSet):
+    queryset = SportClub.objects.prefetch_related("courts__sessions")
+    serializer_class = SportClubSessionsSerializer
