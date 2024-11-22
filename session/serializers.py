5b4c38f2f08d98981d14006474920bb283620144
Olena Zaritska <zaritskaos@gmail.com>
2024-11-22 14:53:53 +0200

    create SportClubSessionsSerializer

--- session/serializers.py
+++ session/serializers.py
@@ -28,3 +28,41 @@ class SportClubSerializer(serializers.ModelSerializer):
     class Meta:
         model = SportClub
         fields = ("id", "name", "location", "working_hours")
+
+
+class SportClubSessionsSerializer(serializers.ModelSerializer):
+    sport_club_id = serializers.IntegerField(source="id")
+    sport_club_name = serializers.CharField(source="name")
+    sessions_by_date = serializers.SerializerMethodField()
+
+    class Meta:
+        model = SportClub
+        fields = ("sport_club_id", "sport_club_name", "sessions_by_date")
+
+    def get_sessions_by_date(self, obj):
+        courts = obj.courts.all()
+        sessions_by_date = {}
+
+        for court in courts:
+            for session in court.sessions.all():
+                date = session.date.strftime("%Y-%m-%d")
+                if date not in sessions_by_date:
+                    sessions_by_date[date] = {}
+
+                court_number = f"court #{court.number}"
+                if court_number not in sessions_by_date[date]:
+                    sessions_by_date[date][court_number] = {
+                        "surface": court.surface,
+                        "court_type": court.court_type,
+                        "sessions": [],
+                    }
+
+                sessions_by_date[date][court_number]["sessions"].append(
+                    {
+                        "id": session.id,
+                        "time_slot": f"{session.start_time.strftime('%H:%M')}-{session.end_time.strftime('%H:%M')}",
+                        "booked": session.booked,
+                    }
+                )
+
+        return sessions_by_date
